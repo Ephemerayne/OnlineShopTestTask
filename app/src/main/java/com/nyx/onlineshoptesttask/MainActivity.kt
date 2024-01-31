7415cfe5b778ba3762cad6072c32bc57da12d58d
ephemerayne <nika_landers@mail.ru>
2024-01-31 15:18:27 +0100

    Initial commit

---
+++ app/src/main/java/com/nyx/onlineshoptesttask/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.nyx.onlineshoptesttask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nyx.onlineshoptesttask.ui.theme.OnlineShopTestTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OnlineShopTestTaskTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OnlineShopTestTaskTheme {
+        Greeting("Android")
+    }
+}
